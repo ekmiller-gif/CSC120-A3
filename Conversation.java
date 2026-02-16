68443d4af33bb0d65f7c513d74cdfdfb58ff5206
ekmiller <ekmiller@smith.edu>
2026-02-15 19:43:12 -0500

    Program takes input for the user, runs the appropriate number of rounds, greets the user, and says goodbye

--- Conversation.java
+++ Conversation.java
@@ -1,21 +1,39 @@
-// You should **not** update any call signatures in this file
-// only modify the body of each function
+
+import java.util.Scanner;
 class Conversation implements ConversationRequirements {
 
   // Attributes 
+  
 
   /**
    * Constructor 
    */
   Conversation() {
-    
+
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    int rounds = 0;
+    
+    Scanner input = new Scanner(System.in);   //Creates new Scanner object for the user to choose the number of rpunds
 
+    System.out.println("How many rounds?");   //Asks the user how many rounds the program will run
+    rounds = input.nextInt();
+
+    
+    System.out.println("Hi there! What's on your mind?");    //Begins the conversation with the user
+
+    Scanner dialogue = new Scanner(System.in);    //Creates new scanner object for conversational input    
+
+    for (int i = 0; i < rounds; i++) {
+        String userResponse = dialogue.nextLine();
+        System.out.println("You said " + userResponse + "?");
+    }
+
+    System.out.println("See ya!");
   }
 
   /**
