7c5f2e493ff484afb100b97c41057d0ae2f71344
ekmiller <ekmiller@smith.edu>
2026-02-17 20:25:43 -0500

    program is integrated into the interface with chat and respond as separate methods and has a functioning print transcript method

--- Conversation.java
+++ Conversation.java
@@ -1,52 +1,60 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-  
+  ArrayList<String> transcript;
 
   /**
    * Constructor 
    */
   Conversation() {
-
+    transcript = new ArrayList<>();
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-    String[] prewrittenResponses = {"That sounds interesting.", "Tell me more.", "Mhmm.", "Great weather we're having!", "Indeed.", "Hmmm."};
-    int responseNumber = prewrittenResponses.length;
 
     int rounds = 0;
-    Random rand = new Random(); //Creates new Random object that we can use to choose random elements
-
-    Scanner input = new Scanner(System.in);   //Creates new Scanner object for the user to choose the number of rpunds
-
     System.out.println("How many rounds?");   //Asks the user how many rounds the program will run
+
+
+    Scanner input = new Scanner(System.in);   //Creates new Scanner object for the user to choose the number of rounds
     rounds = input.nextInt();
 
-    
     System.out.println("Hi there! What's on your mind?");    //Begins the conversation with the user
+    this.transcript.add("Hi there! What's on your mind?");
+    
+    input.nextLine();
 
-    Scanner dialogue = new Scanner(System.in);    //Creates new scanner object for conversational input    
 
     for (int i = 0; i < rounds; i++) {
-        String userResponse = dialogue.nextLine();
-        String l = prewrittenResponses[rand.nextInt(responseNumber)];
-          System.out.println(l);
+      String userResponse = input.nextLine();
+        this.transcript.add(userResponse);
+      String newUserResponse = respond(userResponse);
+        this.transcript.add(respond(newUserResponse));
+      System.out.println(newUserResponse);
+        this.transcript.add(newUserResponse);
     }
 
-    System.out.println("See ya!");
-    
+    System.out.println("See ya!"); //Gives a farewell to the user
+    this.transcript.add("See ya!");
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println();
+    System.out.println("TRANSCRIPT:");
+    for (int i = 0; i < this.transcript.size(); i++) {
+        System.out.println(this.transcript.get(i));
+    }
+    System.out.print("```");
   }
 
   /**
@@ -54,11 +62,55 @@ class Conversation implements ConversationRequirements {
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
    */
-  public String respond(String inputString) {     //I don't understand how this is different form the chat method and if it is how we incorporate the two
+  public String respond(String inputString) {     
     String returnString = "";     
-    return returnString; 
+
+    String[] prewrittenResponses = {"That sounds interesting.", "Tell me more.", "Mhmm.", "Great weather we're having!", "Indeed.", "Hmmm."}; //Defines a list of prewritten responses.
+    int responseNumber = prewrittenResponses.length; //Gives the length (a range of indexes) so that we can choose a random index later
+
+    String[] words = inputString.split(" "); //Splits the response into individual words to taht we can iterate over each word
+    Random rand = new Random(); //Creates new Random object that we can use to choose random elements
+
+    if (inputString.contains("I" ) || inputString.contains("you" )|| inputString.contains("me" ) || inputString.contains("am" ) || inputString.contains("my" ) || inputString.contains("your" )){
+      for (int i = 0; i < words.length; i++){
+        if (words[i].equals("I")){
+          words[i] = "you";
+        }
+        else if (words[i].equals("you")){
+          words[i] = "I";
+        }
+        else if (words[i].equals("me")){
+          words[i] = "you";
+        }
+        else if (words[i].equals("am")){
+          words[i] = "are";
+        }
+        else if (words[i].equals("are")){
+          words[i] = "am";
+        }
+        else if (words[i].equals("my")){
+          words[i] = "your";
+        }
+        else if (words[i].equals("your")){
+          words[i] = "my";
+        }
+      }
+      String sew = " ";
+      returnString = String.join(sew, words) + "?"; //"sews" the string back together
+      
+    }
+    else{
+      String l = prewrittenResponses[rand.nextInt(responseNumber)];
+         System.out.print(l);
+    }
+    return returnString;
+
   }
 
+  
+  
+  
+  
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    I consulted Elizabeth Sarpong and Tho Ngyuen while working on this assignment to brainstorm and debug our programs.
+
 - Which session(s) of TA / office hours did you attend?
+    I attended TA hours on Monday 2/16.
+
 - What are your initial impressions of Java? 
+    While the actual logic of the programming is very similar, I found the syntax much more confusing than in Python when I was first learning it. The syntax in Python is much simpler, and to me at least, a lot more intuitive. I had to look to the java documentation site, Think Java, and geeks for geeks quite a bit. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
